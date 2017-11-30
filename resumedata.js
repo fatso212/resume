4c097569f41f8a47feedb7836277b4ab98d32ca7
tzvi <tzvika200@yahoo.com>
2017-11-30 11:11:39 -0500

    data

---
+++ resumedata.js
@@ -0,0 +1,122 @@
+let personalInfo = {
+	name: "Tzvi Seliger",
+	title: "Full Stack Web Developer",
+	residence:"Pittsburgh, PA 15217",
+	email: "tzviseliger@gmail.com",
+	phone:"(602)-710-5010"
+}	
+
+let about = {
+	shortParagraph: "I was a team leader prior to studying Web Dev." + 
+	"I enjoy directing and working with a team." +
+	"I Iike finding simple solutions that improve efficiency." +
+	"Before Web Development, I was involved in Ecommerce Sales & Customer Service." + 
+	"Authorized to work in the US for any employer",
+}
+
+let WorkExperience = {
+	job1: {
+		title: "Customer Service Dept Head",
+		company:"EMB-phones",
+		employer:"Meir Bukchin",
+		location:"Brooklyn, NY",
+		details:"",
+		start: "June 2015",
+		end: "May 2017"	
+	},
+	job2:{
+		title: "Ecommerce Sales",
+		company: "18th ave. Wireless",
+		employer:"Naftali Feldman",
+		location:"Brooklyn, NY",
+		details:"",
+		start: "June 2015",
+		end: "May 2017"	
+	},
+}
+
+let education = {
+	school1:{
+		subject: "Computer Science"
+		name: "Viking Code School"
+	},
+	school2:{
+
+	}
+}
+
+let technologies = {
+	css:{
+		name: "CSS",
+		time: "2 years"
+	},
+	html:{
+		name: "HTML",
+		time: "2 years"
+	},
+	javascript:{
+		name: "JavaScript",
+		time: "2 years"
+	},
+	jquery:{
+		name: "jQuery",
+		time: "2 years"
+	},
+	php:{
+		name: "PHP",
+		time: "2 years"
+	},
+	templates:{
+		name: "Template Engines",
+		time: "2 years"
+	},
+	node:{
+		name: "NodeJS",
+		time: "2 years"
+	},
+	express:{
+		name: "ExpressJS",
+		time: "2 years"
+	},
+	databases:{
+		name: "Databases",
+		time: "2 years"
+	},
+	marketplaces:{
+		Accountmanagement:{
+			name: "account Manager"
+		},
+		ebay:{
+			name: "ebay",
+			time: "2 years"
+
+		},
+		amazon:{
+			name: "amazon",
+			time: "2 years"
+		}
+	},
+	other:{
+		CS:{
+			name: "Customer Service"
+		},
+		sales: {
+			name: "sales"
+		},
+		teamLead:{
+			name: "team leader"
+		}
+
+	}
+}
+Hasc (Program For Special Needs Individuals) Rabbi Peretz Blasberg - Principal at ULY Elementary
+Additional Information
+Skills
+Listing
+Customer Service
+Phone Manner
+Email, Written Communication (Phone Sales)
+Programming HTML Jade/Pug CSS
+SCSS JavaScript Node Express jQuery Angular PHP
+Ruby
+2 years
